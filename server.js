54eeebf31cd21a031a63783744d4a77661180f2f
patrickatcw <pj8392@yahoo.com>
2017-11-19 13:06:00 -0500

    updated article.js file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var handlebars = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT ||3000;
 var app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/homework", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homework"
+var promise = mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
